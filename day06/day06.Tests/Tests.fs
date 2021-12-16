3f8146d6b0f4d7a1242ec4e5bbb14eebd6ebe9b2
Travis <tssund93@gmail.com>
2021-12-15 21:20:16 -0600

    day 7

--- day06/day06.Tests/Tests.fs
+++ day06/day06.Tests/Tests.fs
@@ -5,30 +5,30 @@ open Xunit
 
 [<Fact>]
 let ``parse input`` () =
-    Assert.Equal<Fish>(parseInput "3,4,3,1,2", [ 0L; 1; 1; 2; 1; 0; 0; 0; 0 ])
+    Assert.Equal<Fish>([ 0L; 1; 1; 2; 1; 0; 0; 0; 0 ], parseInput "3,4,3,1,2")
 
 [<Fact>]
 let ``step`` () =
-    Assert.Equal<Fish>(step [ 0; 1; 1; 2; 1; 0; 0; 0; 0 ], [ 1L; 1; 2; 1; 0; 0; 0; 0; 0 ])
-    Assert.Equal<Fish>(step [ 1; 1; 2; 1; 0; 0; 0; 0; 0 ], [ 1L; 2; 1; 0; 0; 0; 1; 0; 1 ])
-    Assert.Equal<Fish>(step [ 1; 2; 1; 0; 0; 0; 1; 0; 1 ], [ 2L; 1; 0; 0; 0; 1; 1; 1; 1 ])
+    Assert.Equal<Fish>([ 1L; 1; 2; 1; 0; 0; 0; 0; 0 ], step [ 0; 1; 1; 2; 1; 0; 0; 0; 0 ])
+    Assert.Equal<Fish>([ 1L; 2; 1; 0; 0; 0; 1; 0; 1 ], step [ 1; 1; 2; 1; 0; 0; 0; 0; 0 ])
+    Assert.Equal<Fish>([ 2L; 1; 0; 0; 0; 1; 1; 1; 1 ], step [ 1; 2; 1; 0; 0; 0; 1; 0; 1 ])
 
 [<Fact>]
 let ``produces the correct result after a number of steps`` () =
     Assert.Equal<Fish>(
+        26,
         stepTimes 18 [ 0; 1; 1; 2; 1; 0; 0; 0; 0 ]
-        |> Seq.sum,
-        26
+        |> Seq.sum
     )
 
     Assert.Equal<Fish>(
+        5934,
         stepTimes 80 [ 0; 1; 1; 2; 1; 0; 0; 0; 0 ]
-        |> Seq.sum,
-        5934
+        |> Seq.sum
     )
 
     Assert.Equal<Fish>(
+        26984457539L,
         stepTimes 256 [ 0; 1; 1; 2; 1; 0; 0; 0; 0 ]
-        |> Seq.sum,
-        26984457539L
+        |> Seq.sum
     )
---
+++ day07/Program.fs
@@ -0,0 +1,43 @@
+﻿module Day7
+
+open System
+open System.IO
+
+let parseInput (input: string) =
+    input.Trim().Split(',')
+    |> Seq.map int
+    |> List.ofSeq
+
+let positions =
+    File.ReadAllText("input.txt") |> parseInput
+
+let inline cost r = (^T: (member Cost : int) r)
+
+let calcCheapestFuelCost distanceCalc positions =
+    let mutable cheapestPosition, cheapestCost = -1, Int32.MaxValue
+
+    for i in List.min positions .. List.max positions do
+        let cost = positions |> List.sumBy (distanceCalc i)
+
+        if cost < cheapestCost then
+            cheapestCost <- cost
+            cheapestPosition <- i
+
+    {| Position = cheapestPosition
+       Cost = cheapestCost |}
+
+module Part1 =
+    let cheapestFuelCost positions =
+        calcCheapestFuelCost (fun i pos -> abs (pos - i)) positions
+
+    cheapestFuelCost positions
+    |> cost
+    |> printfn "The cheapest amount of fuel that can be spent for all crabs to align is %d"
+
+module Part2 =
+    let cheapestFuelCost positions =
+        calcCheapestFuelCost (fun i pos -> List.init (abs (pos - i)) ((+) 1) |> List.sum) positions
+
+    cheapestFuelCost positions
+    |> cost
+    |> printfn "The cheapest amount of fuel that can be spent for all crabs to align is %d"
