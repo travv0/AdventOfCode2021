54eed3d26b6b4e4c0036d33443d74aabca2147d6
Travis <tssund93@gmail.com>
2021-12-18 02:48:51 -0600

    day 9 part 2

--- day09/Program.fs
+++ day09/Program.fs
@@ -16,8 +16,6 @@ let getAdjacentLocations (x: int) (y: int) (map: int [,]) =
         && adjX < Array2D.length1 map
         && adjY >= 0
         && adjY < Array2D.length2 map)
-    |> Seq.map (fun (adjX, adjY) -> map.[adjX, adjY])
-    |> Seq.toList
 
 module Array2D =
     let transpose array =
@@ -34,19 +32,30 @@ let parseInput (input: string) =
     |> array2D
     |> Array2D.transpose
 
-let markLowPoint map x y _ =
-    let adjacentLocations = getAdjacentLocations x y map
+let findLowPoints map =
+    let markLowPoint map x y _ =
+        let adjacentLocations =
+            getAdjacentLocations x y map
+            |> Seq.map (fun (adjX, adjY) -> map.[adjX, adjY])
 
-    if Seq.forall ((<) map.[x, y]) adjacentLocations then
-        Some(map.[x, y])
-    else
-        None
+        if Seq.forall ((<) map.[x, y]) adjacentLocations then
+            Some(map.[x, y])
+        else
+            None
+
+    let lowPoints = map |> Array2D.mapi (markLowPoint map)
+
+    seq {
+        for x in 0 .. Array2D.length1 map - 1 do
+            for y in 0 .. Array2D.length2 map - 1 do
+                if Option.isSome lowPoints.[x, y] then
+                    yield x, y
+    }
 
 let findRiskLevels map =
     map
-    |> Array2D.mapi (markLowPoint map)
-    |> Seq.cast
-    |> Seq.choose (Option.map ((+) 1))
+    |> findLowPoints
+    |> Seq.map (fun (x, y) -> map.[x, y] + 1)
     |> Seq.toList
 
 let map =
@@ -55,3 +64,34 @@ let map =
 findRiskLevels map
 |> List.sum
 |> printfn "The sum of the risk levels of all low points in the heightmap is %d"
+
+let getBasin x y map =
+    let mutable seen = Set.ofList [ x, y ]
+
+    let getBasinAdjacents x y =
+        getAdjacentLocations x y map
+        |> Seq.filter (fun (adjX, adjY) ->
+            map.[adjX, adjY] <> 9
+            && not (seen |> Set.contains (adjX, adjY)))
+        |> Set.ofSeq
+
+    let mutable locationsToCheck = getBasinAdjacents x y
+
+    while Set.count locationsToCheck > 0 do
+        let locSeq = locationsToCheck |> Set.toSeq
+        let location = Seq.head locSeq
+        locationsToCheck <- Set.union (location ||> getBasinAdjacents) (Seq.tail locSeq |> Set.ofSeq)
+        seen <- Set.add location seen
+
+    List.ofSeq seen
+
+let findThreeLargestBasins map =
+    findLowPoints map
+    |> Seq.map (fun (x, y) -> getBasin x y map |> Seq.length)
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.toList
+
+findThreeLargestBasins map
+|> List.reduce (*)
+|> printfn "The product of the sizes of the three largest basins is %d"
