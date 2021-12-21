21fcc302b24c3591f8f2f6502fa9a5bf7501e4fc
Travis <tssund93@gmail.com>
2021-12-21 10:45:21 -0600

    refactor day 11

--- day11/day11.fsx
+++ day11/day11.fsx
@@ -1,67 +1,70 @@
 ﻿open System
 open System.IO
 
-let mutable flashes = 0
+module Main =
+    let mutable private flashes = 0
 
-let rec incEnergy (octos: int [,]) x y =
-    octos.[x, y] <- octos.[x, y] + 1
+    let rec private incEnergy (octos: int [,]) x y =
+        octos.[x, y] <- octos.[x, y] + 1
 
-    if octos.[x, y] = 10 then
-        flash octos x y
+        if octos.[x, y] = 10 then
+            flash octos x y
 
-and flash octos x y =
-    flashes <- flashes + 1
+    and private flash octos x y =
+        flashes <- flashes + 1
 
-    for flashX in max (x - 1) 0 .. min (x + 1) (Array2D.length1 octos - 1) do
-        for flashY in max (y - 1) 0 .. min (y + 1) (Array2D.length2 octos - 1) do
-            if flashX <> x || flashY <> y then
-                incEnergy octos flashX flashY
+        for flashX in max (x - 1) 0 .. min (x + 1) (Array2D.length1 octos - 1) do
+            for flashY in max (y - 1) 0 .. min (y + 1) (Array2D.length2 octos - 1) do
+                if flashX <> x || flashY <> y then
+                    incEnergy octos flashX flashY
 
-let step (octos: int [,]) =
-    for x in 0 .. Array2D.length1 octos - 1 do
-        for y in 0 .. Array2D.length2 octos - 1 do
-            if octos.[x, y] < 10 then
-                incEnergy octos x y
+    let private step (octos: int [,]) =
+        for x in 0 .. Array2D.length1 octos - 1 do
+            for y in 0 .. Array2D.length2 octos - 1 do
+                if octos.[x, y] < 10 then
+                    incEnergy octos x y
 
-    for x in 0 .. Array2D.length1 octos - 1 do
-        for y in 0 .. Array2D.length2 octos - 1 do
-            if octos.[x, y] > 9 then
-                octos.[x, y] <- 0
+        for x in 0 .. Array2D.length1 octos - 1 do
+            for y in 0 .. Array2D.length2 octos - 1 do
+                if octos.[x, y] > 9 then
+                    octos.[x, y] <- 0
 
-let input = File.ReadAllText("input.txt")
+    let runSteps n octos =
+        flashes <- 0
 
-let parse (input: string) : int [,] =
-    let lines =
-        input.Split(
-            '\n',
-            StringSplitOptions.RemoveEmptyEntries
-            ||| StringSplitOptions.TrimEntries
-        )
+        for _ in 1 .. n do
+            step octos
 
-    array2D [ for line in lines do
-                  [ for c in line do
-                        sprintf "%c" c |> int ] ]
+        flashes
 
-let runSteps n octos =
-    flashes <- 0
+    let input = File.ReadAllText("input.txt")
 
-    for i in 1 .. n do
-        step octos
+    let parse (input: string) : int [,] =
+        let lines =
+            input.Split(
+                '\n',
+                StringSplitOptions.RemoveEmptyEntries
+                ||| StringSplitOptions.TrimEntries
+            )
 
-    flashes
+        array2D [ for line in lines do
+                      [ for c in line do
+                            sprintf "%c" c |> int ] ]
 
-let findSyncronizedFlash octos =
-    let mutable stepCount = 0
-    let mutable syncronized = false
+    let findSyncronizedFlash octos =
+        let mutable stepCount = 0
+        let mutable syncronized = false
 
-    while not syncronized do
-        step octos
-        stepCount <- stepCount + 1
-        syncronized <- octos |> Seq.cast |> Seq.forall ((=) 0)
+        while not syncronized do
+            step octos
+            stepCount <- stepCount + 1
+            syncronized <- octos |> Seq.cast |> Seq.forall ((=) 0)
 
-    stepCount
+        stepCount
 
-let octos () = parse input
+    let octos () = parse input
+
+open Main
 
 octos ()
 |> runSteps 100
@@ -70,3 +73,45 @@ octos ()
 octos ()
 |> findSyncronizedFlash
 |> printfn "The first time all octopuses flash simultaneously is step %d"
+
+module Tests =
+    let input =
+        "5483143223
+    2745854711
+    5264556173
+    6141336146
+    6357385478
+    4167524645
+    2176841721
+    6882881134
+    4846848554
+    5283751526
+    "
+
+    let octos () = parse input
+
+    let run () =
+        printfn
+            "%A"
+            {| Expected = 0
+               Actual = octos () |> runSteps 1 |}
+
+        printfn
+            "%A"
+            {| Expected = 35
+               Actual = octos () |> runSteps 2 |}
+
+        printfn
+            "%A"
+            {| Expected = 204
+               Actual = octos () |> runSteps 10 |}
+
+        printfn
+            "%A"
+            {| Expected = 1656
+               Actual = octos () |> runSteps 100 |}
+
+        printfn
+            "%A"
+            {| Expected = 195
+               Actual = octos () |> findSyncronizedFlash |}
