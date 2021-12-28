979f3049fcba464937c656739add9329f1aa11b9
Travis <tssund93@gmail.com>
2021-12-28 17:11:47 -0600

    day 16

--- day16/day16.ml
+++ day16/day16.ml
@@ -2,6 +2,8 @@ open Base
 open Stdio
 open Printf
 
+let ( >> ) f g x = g (f x)
+
 let file_name =
   match Sys.get_argv () |> Array.to_list with
   | _ :: fn :: _ -> fn
@@ -45,8 +47,36 @@ let hex_to_binary hex : binary =
   String.concat_map hex ~f:hex_digit_to_binary
   |> String.to_list |> List.map ~f:char_to_bit
 
-type packet_data = Literal of int | Operator of packet list
+type packet_data =
+  | Sum of packet list
+  | Product of packet list
+  | Minimum of packet list
+  | Maximum of packet list
+  | Literal of int
+  | GreaterThan of (packet * packet)
+  | LessThan of (packet * packet)
+  | EqualTo of (packet * packet)
+
 and packet = { version : int; data : packet_data }
+
+let rec value packet : int =
+  match packet.data with
+  | Sum sub_packets ->
+      sub_packets |> List.map ~f:value |> List.fold ~init:0 ~f:( + )
+  | Product sub_packets ->
+      sub_packets |> List.map ~f:value |> List.fold ~init:1 ~f:( * )
+  | Minimum sub_packets ->
+      sub_packets |> List.map ~f:value
+      |> List.min_elt ~compare:Int.compare
+      |> Option.value_exn ~message:"no minimum value found"
+  | Maximum sub_packets ->
+      sub_packets |> List.map ~f:value
+      |> List.max_elt ~compare:Int.compare
+      |> Option.value_exn ~message:"no maximum value found"
+  | Literal i -> i
+  | GreaterThan (a, b) -> if value a > value b then 1 else 0
+  | LessThan (a, b) -> if value a < value b then 1 else 0
+  | EqualTo (a, b) -> if value a = value b then 1 else 0
 
 let binary_to_int (binary : binary) =
   Int.of_string ("0b" ^ binary_to_string binary)
@@ -62,7 +92,7 @@ let parse_literal (binary : binary) : int * binary =
   in
   parse_literal' [] binary
 
-let rec parse_operator (binary : binary) : packet list * binary =
+let rec parse_many (binary : binary) : packet list * binary =
   match binary with
   | Zero :: binary ->
       let length_bin, binary = List.split_n binary 15 in
@@ -87,20 +117,45 @@ let rec parse_operator (binary : binary) : packet list * binary =
       in
 
       parse_packets sub_packet_count [] binary
-  | _ -> failwith "parse_operator: empty binary"
+  | _ -> failwith "parse_many: empty binary"
 
 and parse_packet (binary : binary) : packet * binary =
+  let parse_two =
+    parse_many >> function
+    | [ a; b ], c -> ((a, b), c)
+    | l, _ ->
+        failwithf "parse_two: invalid number of elements: %d" (List.length l) ()
+  in
   let v_str, binary = List.split_n binary 3 in
   let version = binary_to_int v_str in
   let t_str, binary = List.split_n binary 3 in
   let data, remaining =
     match binary_to_int t_str with
+    | 0 ->
+        let packet, remaining = parse_many binary in
+        (Sum packet, remaining)
+    | 1 ->
+        let packet, remaining = parse_many binary in
+        (Product packet, remaining)
+    | 2 ->
+        let packet, remaining = parse_many binary in
+        (Minimum packet, remaining)
+    | 3 ->
+        let packet, remaining = parse_many binary in
+        (Maximum packet, remaining)
     | 4 ->
         let literal, remaining = parse_literal binary in
         (Literal literal, remaining)
-    | _ ->
-        let operator, remaining = parse_operator binary in
-        (Operator operator, remaining)
+    | 5 ->
+        let packet, remaining = parse_two binary in
+        (GreaterThan packet, remaining)
+    | 6 ->
+        let packet, remaining = parse_two binary in
+        (LessThan packet, remaining)
+    | 7 ->
+        let packet, remaining = parse_two binary in
+        (EqualTo packet, remaining)
+    | _ -> failwith "parse_packet: bad packet"
   in
   ({ version; data }, remaining)
 
@@ -111,9 +166,14 @@ let rec sum_versions packet =
   packet.version
   +
   match packet.data with
-  | Operator op -> List.map ~f:sum_versions op |> List.fold ~init:0 ~f:( + )
+  | Sum op | Product op | Minimum op | Maximum op ->
+      List.map ~f:sum_versions op |> List.fold ~init:0 ~f:( + )
+  | GreaterThan (a, b) | LessThan (a, b) | EqualTo (a, b) ->
+      sum_versions a + sum_versions b
   | _ -> 0
 
 let () =
   sum_versions packet
-  |> printf "The version sum of the package hierarchy is %d\n"
+  |> printf "The version sum of the package hierarchy is %d\n";
+
+  value packet |> printf "The value of the transmission is %d\n"
