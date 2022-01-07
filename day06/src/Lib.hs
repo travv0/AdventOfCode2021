419185ac71b02724d23d3319d98fbf113bcc9242
Travis <tssund93@gmail.com>
2022-01-07 17:26:54 -0600

    format day 6 code

--- day06/src/Lib.hs
+++ day06/src/Lib.hs
@@ -1,13 +1,17 @@
 module Lib where
 
-import Control.Arrow ((&&&))
-import Data.Char (isSpace)
-import Data.List (group, sort)
-import Data.List.Split (splitOn)
-import Data.Maybe (fromMaybe)
-import Data.Sequence (Seq ((:<|)), (|>))
-import qualified Data.Sequence as S
-import GHC.Exts (fromList)
+import           Control.Arrow                  ( (&&&) )
+import           Data.Char                      ( isSpace )
+import           Data.List                      ( group
+                                                , sort
+                                                )
+import           Data.List.Split                ( splitOn )
+import           Data.Maybe                     ( fromMaybe )
+import           Data.Sequence                  ( Seq((:<|))
+                                                , (|>)
+                                                )
+import qualified Data.Sequence                 as S
+import           GHC.Exts                       ( fromList )
 
 type Fish = Integer
 type Fishes = Seq Fish
@@ -16,15 +20,15 @@ countBy :: (Ord a, Num value) => (a -> key) -> [a] -> [(key, value)]
 countBy f = map (f . head &&& fromIntegral . length) . group . sort
 
 parseInput :: String -> Fishes
-parseInput input =
-  fromList $ map (\i -> fromMaybe 0 . lookup i $ counts) [0 .. 8]
+parseInput input = fromList
+  $ map (\i -> fromMaybe 0 . lookup i $ counts) [0 .. 8]
  where
   fishNums = splitOn "," $ filter (not . isSpace) input
-  counts = countBy read fishNums
+  counts   = countBy read fishNums
 
 step :: Fishes -> Fishes
-step (zeros :<| fishes)
-  | length fishes == 8 = S.adjust' (+ zeros) 6 fishes |> zeros
+step (zeros :<| fishes) | length fishes == 8 =
+  S.adjust' (+ zeros) 6 fishes |> zeros
 step _ = error "where your fishes at?"
 
 stepTimes :: Int -> Fishes -> Fishes
