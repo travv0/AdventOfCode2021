afd66860dd430dbaa76d75e4e8ac83e9522292b3
Travis <tssund93@gmail.com>
2022-01-09 01:29:15 -0600

    day 21 part 2

--- day21/day21.fsx
+++ day21/day21.fsx
@@ -1,9 +1,15 @@
-open System
+open System.Collections.Generic
 open System.IO
 
 type PlayerNum =
     | One
     | Two
+
+module PlayerNum =
+    let switch =
+        function
+        | One -> Two
+        | Two -> One
 
 type Player = { Position: int; Score: int }
 
@@ -22,6 +28,27 @@ type Game =
       WinningScore: int
       Players: Map<PlayerNum, Player> }
 
+type QuantumWins =
+    { PlayerOne: int64
+      PlayerTwo: int64 }
+    static member (+)
+        (
+            { PlayerOne = playerOne1
+              PlayerTwo = playerTwo1 },
+            { PlayerOne = playerOne2
+              PlayerTwo = playerTwo2 }
+        ) =
+        { PlayerOne = playerOne1 + playerOne2
+          PlayerTwo = playerTwo1 + playerTwo2 }
+
+    static member Zero = { PlayerOne = 0; PlayerTwo = 0 }
+
+    static member Max
+        { PlayerOne = playerOne
+          PlayerTwo = playerTwo }
+        =
+        max playerOne playerTwo
+
 module Game =
     let make winningScore playerOnePos playerTwoPos =
         let playerOne = Player.make playerOnePos
@@ -34,7 +61,15 @@ module Game =
             Map.ofList [ (One, playerOne)
                          (Two, playerTwo) ] }
 
-    let roll times game =
+    let updatePlayer playerNum player game : Game =
+        { game with Players = Map.add playerNum player game.Players }
+
+    let getPlayer playerNum game : Player = Map.find playerNum game.Players
+
+    let alterPlayer playerNum f game : Game =
+        { game with Players = Map.change playerNum (Option.map f) game.Players }
+
+    let rollDeterministic times game =
         [ for i in 1 .. times -> (game.Die + i - 1) % 100 + 1 ]
         |> (fun x ->
             List.sum x,
@@ -42,43 +77,105 @@ module Game =
                 Die = List.last x
                 RollCount = game.RollCount + times })
 
-    let playTurn playerNum game =
-        let (spaces, game) = roll 3 game
-        { game with Players = Map.change playerNum (Option.map (Player.move spaces >> Player.updateScore)) game.Players }
+    let playDeterministicTurn playerNum game =
+        let (spaces, game) = rollDeterministic 3 game
+        alterPlayer playerNum (Player.move spaces >> Player.updateScore) game
 
-
-    let play game =
-        let switch =
-            function
-            | One -> Two
-            | Two -> One
-
+    let playDeterministic game =
         let rec loop currentPlayer game =
-            let game = playTurn currentPlayer game
+            let game = playDeterministicTurn currentPlayer game
+            let player = getPlayer currentPlayer game
 
-            if (Map.find currentPlayer game.Players).Score
-               >= game.WinningScore then
-                ((Map.find (switch currentPlayer) game.Players)
-                    .Score,
-                 game.RollCount)
+            let otherPlayer =
+                getPlayer (PlayerNum.switch currentPlayer) game
+
+            if player.Score >= game.WinningScore then
+                (otherPlayer.Score, game.RollCount)
             else
-                loop (switch currentPlayer) game
+                loop (PlayerNum.switch currentPlayer) game
 
         loop One game
+
+    type private CacheKey = (int * PlayerNum * int * int * int * int)
+
+    let private winCache = Dictionary<CacheKey, QuantumWins>()
+
+    let private cacheKey (game: Game) playerNum : CacheKey =
+        let playerOne = getPlayer One game
+        let playerTwo = getPlayer Two game
+        (game.WinningScore, playerNum, playerOne.Position, playerOne.Score, playerTwo.Position, playerTwo.Score)
+
+    let private cacheHit (game: Game) playerNum : option<QuantumWins> =
+        let key = cacheKey game playerNum
+
+        if winCache.ContainsKey(key) then
+            Some winCache.[key]
+        else
+            None
+
+    let private cacheWins game playerNum wins : QuantumWins =
+        let key = cacheKey game playerNum
+
+        if not <| winCache.ContainsKey(key) then
+            winCache.Add(key, wins)
+
+        wins
+
+    let playQuantum (game: Game) : QuantumWins =
+        let rec rollQuantum (game: Game) (playerNum: PlayerNum) (n: int) (sum: int) : QuantumWins =
+            if n > 0 then
+                [ for i in 1 .. 3 -> rollQuantum game playerNum (n - 1) (sum + i) ]
+                |> List.sum
+            else
+                playQuantum' game playerNum (Some sum)
+
+        and playQuantum' (game: Game) (playerNum: PlayerNum) (spaces: option<int>) =
+            let player, game =
+                match spaces with
+                | Some spaces ->
+                    let player =
+                        getPlayer playerNum game
+                        |> Player.move spaces
+                        |> Player.updateScore
+
+                    player, updatePlayer playerNum player game
+                | None -> getPlayer playerNum game, game
+
+            match cacheHit game playerNum with
+            | Some wins -> wins
+            | None ->
+                if player.Score < game.WinningScore then
+                    rollQuantum game (PlayerNum.switch playerNum) 3 0
+                    |> cacheWins game playerNum
+                else
+                    match playerNum with
+                    | One -> { PlayerOne = 1; PlayerTwo = 0 }
+                    | Two -> { PlayerOne = 0; PlayerTwo = 1 }
+
+        playQuantum' game Two None
 
 let calcPart1 playerOnePos playerTwoPos =
     let (losingScore, rollCount) =
         Game.make 1000 playerOnePos playerTwoPos
-        |> Game.play
+        |> Game.playDeterministic
 
     losingScore * rollCount
+
+let calcPart2 playerOnePos playerTwoPos =
+    Game.make 21 playerOnePos playerTwoPos
+    |> Game.playQuantum
+    |> QuantumWins.Max
 
 let parseInput (input: string) =
     match input.Trim().Split('\n') with
     | [| one; two |] -> (one.Split(' ') |> Array.last |> int, two.Split(' ') |> Array.last |> int)
     | _ -> failwith "bad parse"
 
-File.ReadAllText("input.txt")
-|> parseInput
-||> calcPart1
+let playerOnePos, playerTwoPos =
+    File.ReadAllText("input.txt") |> parseInput
+
+calcPart1 playerOnePos playerTwoPos
 |> printfn "The score of the losing player multiplied by the number of times the die was rolled is %d"
+
+calcPart2 playerOnePos playerTwoPos
+|> printfn "The player that wins more with the quantum die wins in %d universes"
